4cf4781e5d53411cf11f43eeb1d0095c888ca573
nezucchi <taken3g@gmail.com>
2025-01-31 12:40:41 +0900

    Delete kadai_002 directory
    課題の提出のためいったん削除

--- kadai_002/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51Qd1JfP4dz5QfHR5JzkRohUoYUry3Jrud0GV97bhlrmV9WrUUXf9VOpIAoMO4rukAHf82BAKUxAKtBGhRb57lHI600SicLXa2w');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
